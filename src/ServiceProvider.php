c5803bbeea175fc5f958de37675b2e7f6a360653
Tjoosten <Tjoosten3@gmail.com>
2016-06-08 23:42:54 +0200

    Add empty service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Idevelopment\Timecontrol\Api;
+
+/**
+ * Class ServiceProvider
+ * @package Idevelopment\Timecontrol\Api
+ */
+class ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void.
+     */
+    public function boot()
+    {
+        // Write your magic here.
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void.
+     */
+    public function register()
+    {
+        
+    }
+}
